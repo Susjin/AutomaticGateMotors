557608a35513d4e58a3b52d4d58c633d24789150
Susjin <pedrohgmoreira@gmail.com>
2023-08-30 21:14:05 -0300

    Updated all translations with new string

--- media/lua/client/AutoGate/AutoGateServerCommands.lua
+++ media/lua/client/AutoGate/AutoGateServerCommands.lua
@@ -5,30 +5,33 @@
 ---	Steam profile: https://steamcommunity.com/id/peter_pg/
 --- GitHub Repository: https://github.com/Susjin/AutomaticGateMotors
 
----	All the methods related to the ServerCommands are listed in this file
+---	All the methods related to the ClientCommands are listed in this file
 --- @class AutoGateServerCommands
 --- @return AutoGateServerCommands
-    local AutoGateServerCommands = {}
+local AutoGateServerCommands = {}
 ----------------------------------------------------------------------------------------------
 --Setting up locals
 local ISAutoGateUtils = require "AutoGate/ISAutoGateUtils"
 
-AutoGateServerCommands.onClientCommand = function(module, command, player, args)
-    if isServer() then
+AutoGateServerCommands.onServerCommand = function(module, command, args)
+    if isClient() then
         if module == "AutoGate" then
             if command == "install" then
                 ISAutoGateUtils.installAutomaticGateMotor(args)
             elseif command == "toggleGate" then
                 ISAutoGateUtils.consumeBatteryMP(args)
+            elseif command == "renameGate" then
+                for i=0, 3 do
+                    local player = getSpecificPlayer(i)
+                    ISAutoGateUtils.renameController(args, nil, nil, player)
+                end
             end
-            sendServerCommand(module, command, args)
-            ISAutoGateUtils.debugMessage(string.format("From player: %s", tostring(player)))
         end
     end
 end
 
---Add functions to events
-Events.OnClientCommand.Add(AutoGateServerCommands.onClientCommand)
+Events.OnServerCommand.Add(AutoGateServerCommands.onServerCommand)
+
 
 ------------------ Returning file for 'require' ------------------
 return AutoGateServerCommands
--- media/lua/server/AutoGate/AutoGateClientCommands.lua
+++ media/lua/server/AutoGate/AutoGateClientCommands.lua
@@ -5,33 +5,30 @@
 ---	Steam profile: https://steamcommunity.com/id/peter_pg/
 --- GitHub Repository: https://github.com/Susjin/AutomaticGateMotors
 
----	All the methods related to the ClientCommands are listed in this file
+---	All the methods related to handling the ClientCommands are listed in this file
 --- @class AutoGateClientCommands
 --- @return AutoGateClientCommands
-local AutoGateClientCommands = {}
+    local AutoGateClientCommands = {}
 ----------------------------------------------------------------------------------------------
 --Setting up locals
 local ISAutoGateUtils = require "AutoGate/ISAutoGateUtils"
 
-AutoGateClientCommands.onServerCommand = function(module, command, args)
-    if isClient() then
+AutoGateClientCommands.onClientCommand = function(module, command, player, args)
+    if isServer() then
         if module == "AutoGate" then
             if command == "install" then
                 ISAutoGateUtils.installAutomaticGateMotor(args)
             elseif command == "toggleGate" then
                 ISAutoGateUtils.consumeBatteryMP(args)
-            elseif command == "renameGate" then
-                for i=0, 3 do
-                    local player = getSpecificPlayer(i)
-                    ISAutoGateUtils.renameController(args, nil, nil, player)
-                end
             end
+            sendServerCommand(module, command, args)
+            ISAutoGateUtils.debugMessage(string.format("From player: %s", tostring(player)))
         end
     end
 end
 
-Events.OnServerCommand.Add(AutoGateClientCommands.onServerCommand)
-
+--Add functions to events
+Events.OnClientCommand.Add(AutoGateClientCommands.onClientCommand)
 
 ------------------ Returning file for 'require' ------------------
 return AutoGateClientCommands
